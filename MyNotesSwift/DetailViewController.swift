3e0b732608ed2f71aa1d384961ce444cdbf05903
Wiley Wimberly <wiley@warmfuzzyapps.com>
2014-08-02 16:28:38 -0400

    Initial Commit

---
+++ MyNotesSwift/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  MyNotesSwift
+//
+//  Created by Wiley Wimberly on 8/2/14.
+//  Copyright (c) 2014 Warm Fuzzy Apps, LLC. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+                            
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
